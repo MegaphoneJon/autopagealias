345b08dd296547499cea49c39fb6c82690800716
Jon Goldberg <jon@megaphonetech.com>
2023-12-20 17:31:55 -0500

    civix upgrade

--- autopagealias.php
+++ autopagealias.php
@@ -14,7 +14,7 @@ function autopagealias_civicrm_post($op, $objectName, $objectId, &$objectRef) {
     return;
   }
   // If this pag isn't active, and/or this event isn't public, don't bother.
-  $pageLookupParams = ['id' => $objectId, 'is_avtive' => 1, 'sequential' => 1];
+  $pageLookupParams = ['id' => $objectId, 'is_active' => 1, 'sequential' => 1];
   if ($objectName == 'Event') {
     $pageLookupParams['is_online_registration'] = 1;
   }
@@ -71,39 +71,12 @@ function autopagealias_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function autopagealias_civicrm_xmlMenu(&$files) {
-  _autopagealias_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function autopagealias_civicrm_install() {
   _autopagealias_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function autopagealias_civicrm_postInstall() {
-  _autopagealias_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function autopagealias_civicrm_uninstall() {
-  _autopagealias_civix_civicrm_uninstall();
 }
 
 /**
@@ -115,83 +88,6 @@ function autopagealias_civicrm_enable() {
   _autopagealias_civix_civicrm_enable();
 }
 
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function autopagealias_civicrm_disable() {
-  _autopagealias_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function autopagealias_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _autopagealias_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function autopagealias_civicrm_managed(&$entities) {
-  _autopagealias_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function autopagealias_civicrm_caseTypes(&$caseTypes) {
-  _autopagealias_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function autopagealias_civicrm_angularModules(&$angularModules) {
-  _autopagealias_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function autopagealias_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _autopagealias_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function autopagealias_civicrm_entityTypes(&$entityTypes) {
-  _autopagealias_civix_civicrm_entityTypes($entityTypes);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -199,9 +95,8 @@ function autopagealias_civicrm_entityTypes(&$entityTypes) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function autopagealias_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
